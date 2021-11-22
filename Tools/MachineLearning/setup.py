75e468baf2d43ee9f1e89c05d52e3810aa16a762
taylanot <taylanozgurturan@gmail.com>
2021-11-22 17:56:54 +0100

    setup ready

--- Tools/MachineLearning/setup.py
+++ Tools/MachineLearning/setup.py
@@ -1,7 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='MachineLearningTools',
-    author="Ozgur Taylan Turan",
-    version='0.0.1',
+   name='MachineLearning',
+   version='0.0.1',
+   description='Machine Learning Toolbox',
+   author='O.T. Turan',
+   author_email='taylanozgurturan@gmail.com',
+   packages=setuptools.find_packages()
 )
