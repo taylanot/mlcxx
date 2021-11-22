e279b3d4016ca9cc2c4bb24c145366db7c34de20
taylanot <taylanozgurturan@gmail.com>
2021-11-22 17:00:42 +0100

    setup fix

--- Tools/Mechanics/setup.py
+++ Tools/Mechanics/setup.py
@@ -1,25 +1,11 @@
-import setuptools
-import os
+from setuptools import setup
 
-# get required information from package
-packageName="Mechanics"                                                         # name of the package
-filePath=os.path.dirname(__file__)                                              # path of this file
-
-# get version
-versionFile=os.path.join(filePath,packageName,"_version.py")                    # version file
-with open(versionFile,"r") as fileHandle:                                       # open version file
-    exec(fileHandle.read())                                                     # -> get version number
-
-# get package description
-readmeFile=os.path.join(filePath,packageName,"README.rst")                      # README file
-with open("README.rst", "r") as fileHandle:                                     # open README file
-    readmeInfo = fileHandle.read()                                              # -> get README information
-
-
-# setup
-setuptools.setup(
-    name=packageName,
-    version=__version__,
-    author="Ozgur Taylan Turan",
-    python_requires='>=3.8',
+setup(
+   name='Mechanics',
+   version='0.0.1',
+   description='Mechanics Toolbox',
+   author='O.T. Turan',
+   author_email='taylanozgurturan@gmail.com',
+   packages=['fenics_'],
+   install_requires=['fenics', 'numpy', 'meshio', ],
 )
