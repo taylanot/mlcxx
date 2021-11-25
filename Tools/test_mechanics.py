641054e747c4da0ad80898d23211eff08d2eb169
taylanot <taylanozgurturan@gmail.com>
2021-11-25 15:07:42 +0100

    test_mechanics modify

--- Tools/test_mechanics.py
+++ Tools/test_mechanics.py
@@ -1,38 +1,49 @@
 from Mechanics.fenics_.src.genrve import *
-#from gmshModel.Model import RandomInclusionRVE
-#import numpy as np
-#import os 
-#
-#
-#initParameters={                                                                
-#    "inclusionSets": [1, 3],                                                   
-#    "inclusionType": "Circle",                                                  
-#    "size": [4, 4, 0],                                                          
-#    "origin": [0, 0, 0],                                                        
-#    "periodicityFlags": [1, 1, 1],                                              
-#    "domainGroup": "domain",                                                    
-#    "inclusionGroup": "inclusions",                                             
-#    "gmshConfigChanges": {"General.Terminal": 0,                                
-#                          "Mesh.CharacteristicLengthExtendFromBoundary": 0,     
-#    }
-#}
-#testRVE=RandomInclusionRVE(**initParameters)
-#
-#
-#modelingParameters={                                                            
-#    "placementOptions": {"maxAttempts": 10000,                                  
-#                         "minRelDistBnd": 0.1,                                  
-#                         "minRelDistInc": 0.1,                                  
-#    }
-#}
-#testRVE.createGmshModel(**modelingParameters)
-#
-#
+from gmshModel.Model import RandomInclusionRVE
+import numpy as np
+import os 
+
+
+initParameters={                                                                
+    "inclusionSets": [1, 3],                                                   
+    "inclusionType": "Circle",                                                  
+    "size": [4, 4, 0],                                                          
+    "origin": [0, 0, 0],                                                        
+    "periodicityFlags": [1, 1, 1],                                              
+    "domainGroup": "domain",                                                    
+    "inclusionGroup": "inclusions",                                             
+    "gmshConfigChanges": {"General.Terminal": 0,                                
+                          "Mesh.CharacteristicLengthExtendFromBoundary": 0,     
+    }
+}
+testRVE=RandomInclusionRVE(**initParameters)
+
+
+modelingParameters={                                                            
+    "placementOptions": {"maxAttempts": 10000,                                  
+                         "minRelDistBnd": 0.1,                                  
+                         "minRelDistInc": 0.1,                                  
+    }
+}
+testRVE.createGmshModel(**modelingParameters)
+
+
+meshingParameters={                                                             
+            "threads": None,                                                            
+            "refinementOptions": {"maxMeshSize": 0.1,                                
+                                  "inclusionRefinement": True,                          
+                                  "interInclusionRefinement": False,                    
+                                  "elementsPerCircumference": 10,                       
+                                  "elementsBetweenInclusions": 10,                       
+                                  "inclusionRefinementWidth": 5,                        
+                                  "transitionElements": "auto",                         
+                                  "aspectRatio": 1.5}}
+
 #testRVE.createMesh(**meshingParameters)
+
+testRVE.saveGeometry("A.step")
+testRVE.close()
+
+#gen = CreateRVEGeometry()
 #
-#testRVE.saveGeometry("A.step")
-#testRVE.close()
-
-gen = CreateRVEGeometry()
-
-gen(0.4)
+#gen(0.4)
