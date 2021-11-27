51b86a665b015e5fc116514358f96eb90d4ee3fc
taylanot <taylanozgurturan@gmail.com>
2021-11-27 19:09:25 +0100

    Minor Change Mechanics-3

--- Students/BSc/2021/AICapstone/gen_rves.py
+++ Students/BSc/2021/AICapstone/gen_rves.py
@@ -8,7 +8,7 @@ from fenics_.src.genrve import *
 #    for i in range(num):
 #        generator(Vf,tag=i+1)
 
-generator = GENERATE_RVE(ext='.msh2')
+generator = GENERATE_RVE(name='Gallery',ext='.msh2')
 Vfs  = np.array([1,2,3,4])/10
 hs  = np.array([0.1,0.2,0.5,1])
 
