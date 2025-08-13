5da71279b2499dcd25ace99978ed22ca49fb598f
taylanot <taylanozgurturan@gmail.com>
2025-08-14 00:13:38 +0200

    fixed some comments in the example

--- lcpp_bits/examples/simple.cpp
+++ lcpp_bits/examples/simple.cpp
@@ -16,61 +16,65 @@ using SAMPLE = data::RandomSelect<>;
 
 int main ( ) 
 {
-  // We will be generating our own data which are two Gaussian blobs 
-  // with identity cocvariance matrices and means placed at -1 and 1
+  // We will be generating our own data with banana dataset aka moons by 
+  // sklearn people
   DATASET data(2); // 2 represents the dimension of the dataset
-  data.Gaussian(100,{-1,1}); // We are drawing 100 samples from each class.
+  data.Banana(200); // We are drawing 100 samples from each class.
+  // You can split this dataset and used training and testing splits in 
+  // LCurve. This operation will be using only the testset to test it.
                              
   // Let's look at the portion of [10,100] of the learning curve
-  auto Ns = arma::regspace<arma::Row<size_t>>(10,1,100);
+  auto Ns = arma::regspace<arma::Row<size_t>>(10,1,350);
   {
     lcurve::LCurve<MODEL,DATASET,
-                   SAMPLE, LOSS,DTYPE> curve(data,        // Provide the dataset
-                                             Ns,          // Training set sizes
-                                             size_t(100), // number of repititions
-                                             true,        // parallel or not?
-                                             true);       // Show the progress 
+                   SAMPLE, LOSS,DTYPE> curve(data,  // dataset
+                                             Ns,  // training set sizes
+                                             size_t(1000),  // repititions
+                                             true,  // parallel 
+                                             true,  // progress 
+                                             "build", // directory
+                                             "fixedLCurve");// name
                                                           
     // This will create a learning curve with 
-    // maxIterations=100, tolerance=1e-6 parameters required by the 
+    // minimumleafsize=50
     // DecisionTree<...>.Train function signiture it should match exactly.
-    // Note that, you have to specify the parameters the way 
     // If you wanted to use deafult parameters you can also do 
     // curve.Generate( )
-    curve.Generate(100,1e-6); 
+    curve.Generate(50); 
     // GetResults returns an arma::Mat<DTYPE> which you can save or do whatever.
     // We will save it for plotting.
-    curve.GetResults().save("nottuned.csv",arma::csv_ascii); 
+    curve.GetResults().save("build/nottuned.csv",arma::csv_ascii); 
   }
 
   {
     lcurve::LCurve<MODEL,DATASET,
-                   SAMPLE, LOSS,DTYPE> curve(data,        // Provide the dataset
-                                             Ns,          // Training set sizes
-                                             size_t(100), // number of repititions
-                                             true,        // parallel or not?
-                                             true);       // Show the progress 
-                                                          
-    // maxIterations range [10,100] with incriments of 10                                                       
-    auto iters = arma::regspace<arma::Row<size_t>>(10,10,100);
+                   SAMPLE, LOSS,DTYPE> curve(data,  // dataset
+                                             Ns,  // training set sizes
+                                             size_t(1000),  // repititions
+                                             true,  // parallel 
+                                             true,  // progress 
+                                             "build", // directory
+                                             "tunedLCurve");// name
+                                             
+    // minimumleafsize range [1,50] with incriments of 5. 
+    auto leafs= arma::regspace<arma::Row<size_t>>(1,5,50);
 
     // Here we will obtain the learning curve with hyper-parameter tuning
     // DecisionTree<...>.Train function signiture it should match exactly.
-    // Note that, you have to specify the parameters the way 
-    // If you wanted to use deafult parameters you can also do 
-    // curve.Generate( )
+    // Here you can also choose to use other cross validation and 
+    // optimization procedures with template paramters of this function
+    // curve.GenerateHpt<CV,OPT> Default is mlpack::SimpleCV and ens::GridSearch
     curve.GenerateHpt(0.2, // validation set percent 
-                      iters); 
+                      leafs// Hyper-parameter values that we search from
+                      ); 
     // GetResults returns an arma::Mat<DTYPE> which you can save or do whatever.
     // We will save it for plotting.
-    curve.GetResults().save("tuned.csv",arma::csv_ascii); 
+    curve.GetResults().save("build/tuned.csv",arma::csv_ascii); 
 
   }
 
-
-
-
   DEPEND_INFO( );
+
   return 0;
 }
 
